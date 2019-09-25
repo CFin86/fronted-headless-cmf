9335dbba3c59726ca0078cc1dd9c632cdd64a485
Chris Finney <chris.finney@pantheon.io>
2019-09-25 13:14:01 -0500

    ugly but kinda works

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//const pTitle = document.getElementById('post-lists');
+//const pContain = document.createElement('div')
+const pContain = document.getElementById('posts-list');
+
+const pBody = document.getElementById('posts-content');
+window.onload = function getWPPosts(){
+    
+    fetch('http://headless-cmf.pantheonlocal.com/wp-json/wp/v2/posts')
+    
+    // response
+    .then(r => r.json())
+    .then(posts => {
+        console.log(posts)
+        posts.map(post => {
+            const pDiv = document.createElement('h2')
+            let pContent = document.createElement('h3')
+
+            pDiv.innerHTML = post.title.rendered;
+            let rawWpContent = post.content.rendered;
+            
+            let renderedContentValue = rawWpContent.replace(/<\/?p[^>]*>/g, "");
+            pContent.innerHTML = renderedContentValue;
+            console.log("this is pContetn", renderedContentValue)
+            pContain.appendChild(pDiv)
+            pBody.appendChild(pContent)
+           // pTitle.innerHTML = post.title.rendered;
+           // pContain.appendChild(pTitle)
+        })
+    })
+}
